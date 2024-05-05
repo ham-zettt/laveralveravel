02310f42fa0c379cd978b69d5527a1edeb0993c3
LabTIA-17 <ilhamzakaria3024@gmail.com>
2024-05-05 19:56:27 +0700

    added files

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <title>Dashboard</title>
+</head>
+<body class="font-['Inter'] bg-gray-200 shadow-lg">
+    @include('partials.navbar')
+</body>
+</html>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="flex justify-between bg-blue-900 text-white px-4">
+    <div class="p-4">
+        <h1 class="font-bold uppercase">Laveral</h1>
+    </div>
+    <div class="flex p-4 gap-4">
+        <a href="" class="hover:text-gray-300 hover:bg-blue-950 rounded-md">Home</a>
+        <a href="" class="hover:text-gray-300 hover:bg-blue-950 rounded-md">Create Data</a>
+        <a href="" class="hover:text-gray-300 hover:bg-blue-950 rounded-md">List Data</a>
+    </div>
+</nav>
